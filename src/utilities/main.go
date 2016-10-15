0e78875310365b3afef02c8835ba8d641a8f1713
Cliff Berg <cliff@cliffberg.com>
2016-10-15 13:41:27 -0400

    misc

--- src/utilities/main.go
+++ src/utilities/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"net/http"
 	// SafeHarbor packages:
-	//"utilities/rest"
+	"utilities/rest"  // we have to list this here or the pkg will not be built.
 )
 
 func main() {
-	
+	// We have to reference the rest package so this will compile.
+	rest.CreateTCPRestContext("", "", 0, "", "",
+		func(*http.Request, string) {})
 }
